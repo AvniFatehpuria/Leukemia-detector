c0d8d4b0f40bdfcbde5da7a5a432df31c2b5a985
AvniFatehpuria <avni2864@gmail.com>
2017-09-22 12:42:16 -0400

    Create leukemia.py

---
+++ leukemia.py
@@ -0,0 +1,55 @@
+from classifiers import *
+from sys import *
+
+def parse():
+    if len(argv)!= 3:
+         print "wrong number of arguments; correct usage: python ..."
+         exit(1)
+    try:
+        num_neighbours = int(argv[1])
+        infile = open(argv[2], "r")
+        plist = []
+        for entry in infile:
+            line = entry.strip()
+            line = line.split(",")
+            plist.append(line)
+        return num_neighbours, plist
+
+    except:
+        print "failed :( :("
+        exit(1)
+
+def output(predictions):
+    truePositives = 0.0
+    trueNegatives = 0.0
+    falsePositives = 0.0
+    falseNegatives = 0.0
+    num_predictions = 0
+    for fold in predictions:
+        for data in fold:
+            num_predictions+=1
+            if data[0][-1] == '+1' or data[0][-1] == '1':
+                if data[1] == 1:
+                    truePositives += 1.0
+                else:
+                    falseNegatives += 1.0
+            else:
+                if data[1] == 1:
+                    falsePositives += 1.0
+                else:
+                    trueNegatives += 1.0
+    accuracy = float((trueNegatives + truePositives)/num_predictions)
+    sensitivity = float(truePositives/(truePositives+falseNegatives))
+    specificity = float(trueNegatives/(trueNegatives+falsePositives))
+    print "Results:\n\nAccuracy: %.2f \nSensitivity: %.2f \nSpecificity: %.2f" % (accuracy, sensitivity, specificity)
+    return
+def main():
+    numFolds = 5
+    (num_neighbours, plist) = parse()
+    (trainingFolds, testFolds) = generateFolds(numFolds, plist)
+    prediction_list = []
+    for i in range(len(trainingFolds)):
+        prediction_list.append(kNearestNeighbours(trainingFolds[i], testFolds[i], num_neighbours))
+    output(prediction_list)
+    #print prediction_list
+main()
