e1f5aa5063c5cb5c0ae709cb31bc72bcfc291b74
Avni Fatehpuria <avni2864@gmail.com>
2018-01-05 16:46:05 +0530

    Update leukemia.py

--- leukemia.py
+++ leukemia.py
@@ -2,8 +2,9 @@ from classifiers import *
 from sys import *
 
 def parse():
+    """ parses command line arguments and returns k and the data in the input file as a list of lists"""
     if len(argv)!= 3:
-         print "wrong number of arguments; correct usage: python ..."
+         print "wrong number of arguments; correct usage: python leukemia-detector k filename"
          exit(1)
     try:
         num_neighbours = int(argv[1])
@@ -16,10 +17,11 @@ def parse():
         return num_neighbours, plist
 
     except:
-        print "failed :( :("
+        print "failed to process input file"
         exit(1)
 
 def output(predictions):
+    """takes a list of predictions across folds and calculates _ prints the accuracy, sensitivity and specificity of the prediction"""
     truePositives = 0.0
     trueNegatives = 0.0
     falsePositives = 0.0
@@ -43,6 +45,7 @@ def output(predictions):
     specificity = float(trueNegatives/(trueNegatives+falsePositives))
     print "Results:\n\nAccuracy: %.2f \nSensitivity: %.2f \nSpecificity: %.2f" % (accuracy, sensitivity, specificity)
     return
+
 def main():
     numFolds = 5
     (num_neighbours, plist) = parse()
